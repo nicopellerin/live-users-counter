6c43ce71bfbd3de11b65ca347d9a0f78b524ae93
Nico Pellerin <nickypellerin@gmail.com>
2024-05-17 12:32:25 -0400

    Add ip address check

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,90 @@
 use axum::routing::get;
 use axum::Router;
-use serde_json::Value;
+use local_ip_address::local_ip;
 use socketioxide::{
     extract::{Data, SocketRef},
     SocketIo,
 };
+use std::collections::HashSet;
 use std::sync::atomic::AtomicUsize;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
-use tracing::info;
 
 struct UserCounter {
     count: AtomicUsize,
+    ips: Mutex<HashSet<String>>,
 }
 
 impl UserCounter {
     fn new() -> Self {
         Self {
             count: AtomicUsize::new(0),
+            ips: Mutex::new(HashSet::new()),
         }
     }
 
-    fn increment(&self) -> usize {
+    fn increment(&self, ip: String) -> usize {
+        let mut ips = self.ips.lock().unwrap();
+
+        if ips.contains(&ip) {
+            return self.count.load(std::sync::atomic::Ordering::SeqCst);
+        }
+
+        ips.insert(ip.clone());
+
         self.count.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1
     }
 
-    fn decrement(&self) -> usize {
+    fn decrement(&self, ip: String) -> usize {
+        let mut ips = self.ips.lock().unwrap();
+
+        if ips.is_empty() {
+            return self.count.load(std::sync::atomic::Ordering::SeqCst);
+        }
+
+        if ips.contains(&ip) {
+            ips.remove(&ip);
+            return self.count.load(std::sync::atomic::Ordering::SeqCst);
+        }
+
         self.count.fetch_sub(1, std::sync::atomic::Ordering::SeqCst) - 1
     }
 }
 
 async fn on_connect(socket: SocketRef, user_counter: Arc<UserCounter>) {
-    let user_count = user_counter.increment();
+    let ip = local_ip().unwrap();
 
-    info!(
-        "Client connected: {}, live users: {}",
-        socket.id, user_count
-    );
+    let user_count = user_counter.increment(ip.to_string());
 
-    socket.broadcast().emit("live_users", &user_count).unwrap();
+    // println!(
+    //     "Client connected: {}, live users: {}",
+    //     socket.id, user_count
+    // );
 
-    let socket_clone = socket.clone();
+    socket.on("get_live_users", move |socket: SocketRef| {
+        // println!("get_live_users: {}", user_count);
 
-    socket.on_disconnect(move || {
-        let user_count = user_counter.decrement();
+        socket.emit("live_users", &user_count).unwrap();
 
-        info!(
-            "Client disconnected: {}, live users: {}",
-            socket_clone.id, user_count
-        );
+        socket.broadcast().emit("live_users", &user_count).unwrap();
+    });
 
-        socket_clone
-            .broadcast()
-            .emit("live_users", &user_count)
-            .unwrap();
+    socket.on_disconnect(move |socket: SocketRef| {
+        let user_count = user_counter.decrement(ip.to_string());
+
+        // println!(
+        //     "Client disconnected: {}, live users: {}",
+        //     socket.id, user_count
+        // );
+
+        socket.broadcast().emit("live_users", &user_count).unwrap();
     })
 }
 
 // #[tokio::main]
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-
     let (layer, io) = SocketIo::new_layer();
     let user_counter = Arc::new(UserCounter::new());
 
@@ -78,6 +101,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     // let listener = tokio::net::TcpListener::bind("127.0.0.1:1337").await?;
     //
     // axum::serve(listener, app).await?;
+
+    // Ok(())
 
     Ok(app.into())
 }
