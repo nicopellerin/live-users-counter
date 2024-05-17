2978f8aa619d7cacea6c56b5f80b1186eedc4d6e
Nico Pellerin <nickypellerin@gmail.com>
2024-05-17 19:18:20 -0400

    Change logic

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,6 @@ impl UserCounter {
     fn increment(&self, ip: String) -> usize {
         let mut ips = self.ips.lock().unwrap();
 
-        // println!("IP: {:?}", ips);
-
         if ips.contains(&ip) {
             return self.count.load(std::sync::atomic::Ordering::SeqCst);
         }
@@ -52,42 +50,79 @@ impl UserCounter {
 
         self.count.fetch_sub(1, std::sync::atomic::Ordering::SeqCst) - 1
     }
+
+    // fn get_count(&self) -> usize {
+    //     self.count.load(std::sync::atomic::Ordering::SeqCst)
+    // }
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct User {
+    ip: String,
 }
 
 async fn on_connect(socket: SocketRef, user_counter: Arc<UserCounter>) {
-    let ip = local_ip().unwrap();
+    // let ip = local_ip().unwrap();
 
-    let user_count = user_counter.increment(ip.clone().to_string());
+    // let user_count = user_counter.increment(ip.clone().to_string());
 
-    println!(
-        "Client connected: {}, live users: {}",
-        socket.id, user_count
+    // println!(
+    //     "Client connected: {}, live users: {}",
+    //     socket.id, user_count
+    // );
+    //
+
+    let mut ip = Arc::new(String::new());
+
+    // if socket.connected() {
+    let user_counter_clone = user_counter.clone();
+    socket.on(
+        "get_live_users",
+        move |socket: SocketRef, Data::<User>(user)| {
+            let user_count = user_counter.increment(user.ip.to_string());
+
+            ip = Arc::new(user.ip.to_string());
+            
+            println!("get_live_users: {:?}", user);
+            // println!("ip: {}", user.ip);
+
+            socket.emit("live_users", &user_count).unwrap();
+
+            socket.broadcast().emit("live_users", &user_count).unwrap();
+        },
     );
 
-    socket.on("get_live_users", move |socket: SocketRef| {
-        println!("get_live_users: {}", user_count);
 
-        socket.emit("live_users", &user_count).unwrap();
+    socket.on(
+        "remove",
+        move |socket: SocketRef, Data::<User>(user)| {
+            let user_count = user_counter_clone.decrement(user.ip.to_string());
 
-        socket.broadcast().emit("live_users", &user_count).unwrap();
-    });
+            println!("remove: {:?}", user);
+            // println!("ip: {}", user.ip);
 
-    let user_counter_clone = Arc::clone(&user_counter);
-    socket.on_disconnect(move |socket: SocketRef| {
-        let user_count = user_counter_clone.decrement(ip.clone().to_string());
+            socket.emit("live_users", &user_count).unwrap();
 
-        println!(
-            "Client disconnected: {}, live users: {}",
-            socket.id, user_count
-        );
+            socket.broadcast().emit("live_users", &user_count).unwrap();
+        },
+    );
 
-        socket.broadcast().emit("live_users", &user_count).unwrap();
-    })
+    // socket.on_disconnect(move |socket: SocketRef| {
+    //     let user_count = user_counter_clone.decrement(ip.clone().to_string());
+    // 
+    //     println!(
+    //         "Client disconnected: {}, live users: {}",
+    //         socket.id, user_count
+    //     );
+    // 
+    //     socket.broadcast().emit("live_users", &user_count).unwrap();
+    // })
 }
 
 // #[tokio::main]
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main()-> shuttle_axum::ShuttleAxum
+{
     let (layer, io) = SocketIo::new_layer();
     let user_counter = Arc::new(UserCounter::new());
 
@@ -95,6 +130,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        // .with_state(io)
         .layer(
             ServiceBuilder::new()
                 .layer(CorsLayer::permissive())
@@ -102,9 +138,9 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         );
 
     // let listener = tokio::net::TcpListener::bind("127.0.0.1:1337").await?;
-    //
+    // 
     // axum::serve(listener, app).await?;
-    //
+    // 
     // Ok(())
 
     Ok(app.into())
